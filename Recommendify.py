0398693ab141c59c3e0a506492da002a7ec66038
baronwoy <woybaron@gmail.com>
2024-04-02 22:39:02 +0100

    Imported tkinter and spotipy, set the title of the main app window, added placeholder to ensure correct operation of program

---
+++ Recommendify.py
@@ -0,0 +1,13 @@
+# ~ Import tkinter and spotipy ~ #
+from tkinter import *
+from tkinter import ttk
+import spotipy
+
+# ~ Sets the main application window
+root = Tk()
+# ~ Sets the title of the application ~ #
+root.title("Recommendify")
+
+# ~ Placeholder to ensure tkinter is working ~ #
+ttk.Button(root, text="Hello World").grid()
+root.mainloop()
