0aca6bfe3c084bc5c05f2f7aaa221d5fb5ac8812
baronwoy <woybaron@gmail.com>
2024-04-03 14:38:04 +0100

    Imported local environment files

--- Recommendify.py
+++ Recommendify.py
@@ -1,13 +1,18 @@
-# ~ Import tkinter and spotipy ~ #
+# ~ Import tkinter and spotipy and env libraries
 from tkinter import *
 from tkinter import ttk
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+# ~ gets local environment files for CLIENT ID, CLIENT SECRET and REDIRECT URI
+load_dotenv()
 
 # ~ Sets the main application window
 root = Tk()
-# ~ Sets the title of the application ~ #
+# ~ Sets the title of the application
 root.title("Recommendify")
 
-# ~ Placeholder to ensure tkinter is working ~ #
+# ~ Placeholder to ensure tkinter is working
 ttk.Button(root, text="Hello World").grid()
 root.mainloop()
