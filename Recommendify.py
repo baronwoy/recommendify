76e5df87760759cf3508e0d592cf92da3bcd37ca
baronwoy <woybaron@gmail.com>
2024-04-07 22:10:11 +0100

    add main frame, basic labels and buttons

--- Recommendify.py
+++ Recommendify.py
@@ -14,6 +14,23 @@ root = Tk()
 root.title("Recommendify")
 root.geometry("1120x469")
 
-# ~ Placeholder to ensure tkinter is working
-ttk.Button(root, text="Hello World").grid()
+# ~ Sets the main frame of the application
+mainframe = ttk.Frame(root, padding='3 3 12 12')
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# ~ Labels of text
+ttk.Label(mainframe, text='Top 5 Songs of the past 12 months').grid(column=1, row=1, sticky=(W, S))
+ttk.Label(mainframe, text='Top 5 Artists of the past 12 months').grid(column=2, row=1, sticky=(W, S))
+ttk.Label(mainframe, text='5 Song Recommendations').grid(column=1, row=3, sticky=(W, S))
+ttk.Label(mainframe, text='5 Artist Recommendations').grid(column=2, row=3, sticky=(W, S))
+
+# ~ Buttons
+ttk.Button(mainframe, text='Generate Playlist on Songs').grid(column=1, row=5, sticky=(W, E))
+ttk.Button(mainframe, text='Generate Playlist on Artists').grid(column=2, row=5, sticky=(W, E))
+
+# ~ Adds padding to all the widgets
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5,pady=5)
 root.mainloop()
